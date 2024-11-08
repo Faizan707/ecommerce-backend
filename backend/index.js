70493fbb77006759bb547a7321b9070f11a22b25
Faizan707 <faizanbutt707@gmail.com>
2024-11-09 03:13:09 +0500

    change in index.js

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,11 @@ import dotenv from "dotenv"
  const PORT = process.env.PORT
 databaseConnection();
 app.use('/uploads', express.static('uploads'));
-app.use(cors())
-app.use(express.json()); 
+app.use(cors({
+    origin: '*', 
+    methods: 'GET,POST,PUT,DELETE', 
+  }));
+  app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/admin-login", adminRoutes);
 app.use("/api/",userRoutes)
