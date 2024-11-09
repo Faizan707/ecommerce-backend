1151ffb8996134655bf18fe634e2486937ccf8fb
Faizan707 <faizanbutt707@gmail.com>
2024-11-09 17:38:01 +0500

    change in index and getallproduct

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,34 +1,55 @@
 import express from "express";
-import {databaseConnection} from "../config/db.js"
+import { databaseConnection } from "../config/db.js";
 import adminRoutes from "../routes/admin.routes.js";
-import userRoutes from "../routes/user.routes.js"
-import productRoutes from "../routes/porduct.routes.js"
-import categoryRoutes from "../routes/category.routes.js"
-import orderRoutes from "../routes/order.routes.js"
-import cors from "cors"
+import userRoutes from "../routes/user.routes.js";
+import productRoutes from "../routes/porduct.routes.js";
+import categoryRoutes from "../routes/category.routes.js";
+import orderRoutes from "../routes/order.routes.js";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
-import dotenv from "dotenv"
+const PORT = process.env.PORT;
 
- dotenv.config()
-
- const PORT = process.env.PORT
+// Connect to database
 databaseConnection();
+
+// Serve static files from 'uploads' directory
 app.use('/uploads', express.static('uploads'));
+
+// Enable CORS
 let corsOptions = {
   origin: '*',
-  optionsSuccessStatus: 200 
-}
+  optionsSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 
+// Measure request time
+app.use((req, res, next) => {
+  const start = Date.now(); // Record the start time
 
-  app.use(express.json()); 
+  res.on('finish', () => {
+    const end = Date.now(); // Record the end time
+    const timeTaken = end - start;
+    console.log(`${req.method} ${req.originalUrl} took ${timeTaken}ms`);
+  });
+
+  next(); // Continue to the next middleware or route handler
+});
+
+// Middleware for parsing request bodies
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/admin-login", adminRoutes);
-app.use("/api/",userRoutes)
-app.use("/api/products",productRoutes)
-app.use("/api/",categoryRoutes)
-app.use("/api/order",orderRoutes)
 
-app.listen(PORT,()=>{
-    console.log("connect to port",PORT)
-})
+// Routes
+app.use("/api/admin-login", adminRoutes);
+app.use("/api/", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/", categoryRoutes);
+app.use("/api/order", orderRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log("Connected to port", PORT);
+});
--- backend/controllers/getAllProducts.js
+++ backend/controllers/getAllProducts.js
@@ -3,7 +3,7 @@ import Product from "../models/product.js";
 
 export const getAllProducts = async (req, res) => {
     try {
-      const products = await Product.find().populate('category', 'name'); 
+      const products = await Product.find().populate('category', 'name').lean();
       res.status(200).json(products);
     } catch (error) {
       res.status(500).json({ message: "Server error", error });
