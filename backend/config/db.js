0b7c58b78276936ccb155a587752e9d226df76eb
Faizan707 <faizanbutt707@gmail.com>
2024-11-11 10:46:52 +0500

    change in db file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,16 @@
 import dontenv from "dotenv"
 import mongoose from "mongoose";
+import logger from "../logger.js";
 dontenv.config();
 
 export const databaseConnection = async() =>{
-    try{
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log("connected to database")
-    }catch(e){
-        console.log(e.error)
-
+    try {
+        const db = await mongoose.connect(process.env.MONGO_URL, {
+        });
+        logger.info(`Connected to database at ${db.connection.host}`);
+    } catch (error) {
+        logger.error("Error connecting to database:", { error: error.message });
+        process.exit(1);  // Exit the process on failure
     }
-}
+};
+
